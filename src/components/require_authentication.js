e1c98c9e664bc19f1723c6caa75c537fdb2a2c61
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-22 13:54:16 +0200

    hoc finit

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
 
-export default function(ComposedComponent){
-    class Authentication extends Component{
-        reder(){
+export default function (ComposedComponent) {
+
+    Component.propTypes = {
+        name: PropTypes.string,
+        authArray: PropTypes.arrayOf(PropTypes.string),
+    };
+
+    class Authentication extends Component {
+        static contextTypes = {
+            router: PropTypes.object
+        };
+
+
+        componentWillMount() {
+            if (!this.props.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+
+        componentWillUpdate(nextProps, nextState) {
+            if (!nextProps.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+
+        render() {
             return <ComposedComponent {...this.props}/>
         }
     }
 
-    return Authentication;
+    function mapStateToProps(state) {
+        return {
+            authenticated: state.authenticated
+        }
+    }
+
+    return connect(mapStateToProps)(Authentication);
 }
